dd751c668bada3d784c93edd88ea1fc6fe1f9d66
Gabi Dobocan <gabi@telepat.io>
2023-02-27 20:11:29 +0200

    feat: move graph code to audit lib

--- src/files/index.js
+++ src/files/index.js
@@ -1,12 +1,5 @@
-const path = require('path');
-const lockfiles = require('./lockfiles');
 const sandworm = require('./sandworm');
-const packages = require('./packages');
-const {loadJsonFile} = require('./utils');
 
 module.exports = {
-  ...lockfiles,
   ...sandworm,
-  ...packages,
-  loadManifest: (appPath) => loadJsonFile(path.join(appPath, 'package.json')),
 };
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const logger = require('./logger');
 const permissions = require('./permissions');
 const recorder = require('./recorder');
 const sandworm = require('./sandworm');
-const getDependencyGraph = require('./graph');
-const {addDependencyGraphData} = require('./graph/utils');
 
 module.exports = {
   files,
@@ -12,6 +10,4 @@ module.exports = {
   permissions,
   recorder,
   sandworm,
-  getDependencyGraph,
-  addDependencyGraphData,
 };
